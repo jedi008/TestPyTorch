b6cbd55eab271843b60ca4cb4fd0b034329f403c
Jie Li <32835610+jedi007@users.noreply.github.com>
2022-09-15 14:28:13 +0800

    add DQN demo (#16)
    * add ddpg demo

--- RL/GoBang/train.py
+++ RL/GoBang/train.py
@@ -9,9 +9,10 @@ import torch.optim as optim
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print("device: ", device)
 #Hyperparameters
-learning_rate = 0.01
+learning_rate = 0.005
 gamma = 0.99
-episodes = 100000
+start = 200001
+episodes = start + 2000000
 render = False
 SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
 
@@ -20,6 +21,10 @@ env = ENV(block_size, device)
 
 model_black = Actor(state_space=block_size**2, action_space=block_size**2)
 model_white = Actor(state_space=block_size**2, action_space=block_size**2)
+
+model_black.load_state_dict(torch.load("GoBang_Model/ModelTraing200000_black.pt"))
+model_white.load_state_dict(torch.load("GoBang_Model/ModelTraing200000_white.pt"))
+
 model_black.to(device)
 model_white.to(device)
 optimizer_black = optim.Adam(model_black.parameters(), lr=learning_rate)
@@ -55,26 +60,23 @@ def finish_episode(model, optimizer):
     del model.rewards[:]
     del model.save_actions[:]
 
+
+# 保证和a相同的维度大小
+zero = torch.zeros((1,225), device=device)
+one = torch.ones((1,225), device=device)
+half = one * 0.5
+
 def select_action(state, model):
     probs, state_value = model(state)
-    # print("probs 1: ",probs)
-
-    # 保证和a相同的维度大小
-    zero = torch.zeros((1,225), device=device)
-    one = torch.ones((1,225), device=device)
 
     x0 = state.clone().view(1,225)
-    a = torch.where(x0 > 0.4, one * 0.5, x0) # 已经落子的位置设置为0.5
+    a = torch.where(x0 > 0.4, half, x0) # 已经落子的位置设置为0.5
     a = torch.where(a < 0.4, one, a) # 还可以落子的空位设置为1
     a = torch.where(a < 0.6, zero, a) # 已经落子的位置0
 
     probs = probs.clone() * a
-    # print("a: ",a)
-    # print("probs 2: ",probs)
-    # print("probs.sum(): ", probs.sum())
     probs /= probs.sum()
 
-    # print("probs 3: ",probs)
     m = Categorical(probs)
     action = m.sample()
     model.save_actions.append(SavedAction(m.log_prob(action), state_value.view(1)))
@@ -82,48 +84,104 @@ def select_action(state, model):
     row = int(action.item() / block_size)
     col = int(action.item() % block_size)
 
-    # print("row, col: ",row, col)
     return row, col
 
-def main():
-    for i_episode in range(episodes):
-        state = env.reset()
-        player = env.player
-        for t in count():
-            # 黑棋落子
-            if env.board.sum().item() == 0:
-                row = int(env.block_size/2)
-                col = int(env.block_size/2)
-            else:
-                row,col = select_action(state.view(1, 1, block_size, block_size), model_black)
-            state, reward_black, player, info = env.step(player, row, col, render)
-            if env.done:
-                model_black.rewards.append(reward_black)
-                model_white.rewards.append(-2000)
-                break
+def select_best_action(state, model):
+    probs, state_value = model(state)
 
-            # 白棋响应
-            row, col = select_action(state.view(1, 1, block_size, block_size), model_white)
-            state, reward_white, player, info = env.step(player, row, col, render)
-            if env.done:
-                model_black.rewards.append(-2000)
-                model_white.rewards.append(reward_white)
-                break
+    x0 = state.clone().view(1,225)
+    a = torch.where(x0 > 0.4, half, x0) # 已经落子的位置设置为0.5
+    a = torch.where(a < 0.4, one, a) # 还可以落子的空位设置为1
+    a = torch.where(a < 0.6, zero, a) # 已经落子的位置0
 
-            if env.board.sum().item() > 2: # 小于2意味着是第一步落子，没有经过network
-                model_black.rewards.append(reward_black)
-            model_white.rewards.append(reward_white)
+    probs = probs.clone() * a
+
+    action = torch.argmax(probs)
+
+    row = int(action.item() / block_size)
+    col = int(action.item() % block_size)
+
+    return row, col
+
+def train_black(i_episode):
+    model_black.train()
+    model_white.eval()
+
+    state = env.reset()
+    player = env.player
+    for t in count():
+        # 黑棋落子
+        if env.board.sum().item() == 0:
+            row = int(env.block_size/2)
+            col = int(env.block_size/2)
+        else:
+            row,col = select_action(state.view(1, 1, block_size, block_size), model_black)
+        state, reward_black, player, info = env.step(player, row, col, render)
+        if env.done:
+            model_black.rewards.append(reward_black)
+            break
         
-        if env.winer != None and i_episode % 100 == 0:
-            env.show()
-            print("i_episode: {} == {} steps, winner is {} @{}-{}".format(i_episode, t, env.winer, row+1, col+1))
+        # 白棋响应
+        row, col = select_best_action(state.view(1, 1, block_size, block_size), model_white)
+        state, reward_white, player, info = env.step(player, row, col, render)
+        if env.done:
+            model_black.rewards.append(-reward_white)
+            break
 
-        finish_episode(model_black, optimizer_black)
-        finish_episode(model_white, optimizer_white)
+        if env.board.sum().item() > 2: # 小于2意味着是第一步落子，没有经过network
+            model_black.rewards.append(reward_black)
+
+    if env.winer != None and i_episode % 100 == 0:
+        env.show()
+        print("train_black i_episode: {} == {} steps, winner is {} @{}-{}".format(i_episode, t, env.winer, row+1, col+1))
+    finish_episode(model_black, optimizer_black)
+
+def train_white(i_episode):
+    model_black.eval()
+    model_white.train()
+
+    state = env.reset()
+    player = env.player
+    for t in count():
+        # 黑棋落子
+        if env.board.sum().item() == 0:
+            row = int(env.block_size/2)
+            col = int(env.block_size/2)
+        else:
+            row,col = select_best_action(state.view(1, 1, block_size, block_size), model_black)
+        state, reward_black, player, info = env.step(player, row, col, render)
+        if env.done:
+            model_white.rewards.append(-reward_black)
+            break
+
+        # 白棋响应
+        row, col = select_action(state.view(1, 1, block_size, block_size), model_white)
+        state, reward_white, player, info = env.step(player, row, col, render)
+        if env.done:
+            model_white.rewards.append(reward_white)
+            break
+        
+        model_white.rewards.append(reward_white)
     
-    modelPath = './GoBang_Model/ModelTraing'+str(episodes)
-    torch.save(model_black.state_dict(), modelPath+'_black.pt')
-    torch.save(model_white.state_dict(), modelPath+'_white.pt')
+    if env.winer != None and i_episode % 100 == 0:
+        env.show()
+        print("train_white i_episode: {} == {} steps, winner is {} @{}-{}".format(i_episode, t, env.winer, row+1, col+1))
+
+    finish_episode(model_white, optimizer_white)
+
+
+def main():
+    for i_episode in range(start, episodes):
+        if i_episode % 3000 < 1000:
+            train_black(i_episode)
+        else:
+            train_white(i_episode)
+    
+        if i_episode % 10000 == 0:
+            modelPath = './GoBang_Model/ModelTraing'+str(i_episode)
+            torch.save(model_black.state_dict(), modelPath+'_black.pt')
+            torch.save(model_white.state_dict(), modelPath+'_white.pt')
+            
 
 if __name__ == '__main__':
     main()
